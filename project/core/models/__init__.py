558c26c6ca94d657548c425711fdb734d9288361
태라 <terra2007@naver.com>
2022-10-16 23:40:49 +0900

    📑 :: Entity initialize

---
+++ project/core/models/__init__.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+import redis
+
+from project.config import MYSQL_DB_URL
+
+@contextmanager
+def session_scope():
+    engine = create_engine(
+        MYSQL_DB_URL,
+        encoding="utf-8",
+        pool_recycle=3600,
+        pool_size=20,
+        max_overflow=20,
+        pool_pre_ping=True,
+        echo=True
+    )
+    Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+Base = declarative_base()
+
+Redis = redis.StrictRedis(host='localhost', port=6379, db=0)
