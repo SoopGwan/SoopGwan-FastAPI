ec102d4bd13c6dba26abc1a0c2e3f4bac937ff41
태라 <terra2007@naver.com>
2022-10-19 01:16:40 +0900

    🐛 :: User Entity Modify

--- project/core/models/user.py
+++ project/core/models/user.py
@@ -1,24 +1,12 @@
-from sqlalchemy import Column, VARCHAR, CHAR, BigInteger
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import BigInteger, CHAR, Column, String, text
 from project.core.models import Base
-from project.core.models.achive import Achive
-from project.core.models.achive_success import Achive_Success
-from project.core.models.habit import Habit
-from project.core.models.habit_success import Habit_Success
 
+class TblUser(Base):
+    __tablename__ = 'tbl_user'
 
-class User(Base):
-    __tablename__ = "user"
-
-    id = Column(BigInteger, primary_key=True, autoincrement=True)
-    account_id = Column(VARCHAR(24), nullable=False)
+    id = Column(BigInteger, primary_key=True, server_default=text("'0'"))
+    account_id = Column(String(24), nullable=False)
     password = Column(CHAR(60), nullable=False)
-    name = Column(VARCHAR(10), nullable=False)
-    phone_number = Column(VARCHAR(11), nullable=False)
+    name = Column(String(10), nullable=False)
+    phone_number = Column(CHAR(11), nullable=False)
     level = Column(CHAR(3), nullable=False)
-
-    achive = relationship(Achive, backref="user")
-    achive_success = relationship(Achive_Success, backref="user")
-    habit = relationship(Habit, backref="user")
-    habit_success = relationship(Habit_Success, backref="user")
