9bcb1797a4868c0e31b5872c49f4acf34760bdff
태라 <terra2007@naver.com>
2022-10-20 23:32:43 +0900

    🐛 :: Achive Entity Modify

--- project/core/models/achive.py
+++ project/core/models/achive.py
@@ -1,13 +1,13 @@
-from sqlalchemy import BigInteger, Column, ForeignKey, String, text
+from sqlalchemy import BIGINT, Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from project.core.models import Base
 
-class TblAchive(Base):
+class Achive(Base):
     __tablename__ = 'tbl_achive'
 
-    id = Column(BigInteger, primary_key=True, server_default=text("'0'"))
+    id = Column(BIGINT, primary_key=True, autoincrement=True)
     condition = Column(String(200), nullable=False)
     title = Column(String(10), nullable=False)
-    user_id = Column(ForeignKey('tbl_user.id'), nullable=False, index=True, server_default=text("'0'"))
+    user_id = Column(ForeignKey('user.id'), nullable=False, index=True)
 
-    user = relationship('TblUser')
+    user = relationship('User')
