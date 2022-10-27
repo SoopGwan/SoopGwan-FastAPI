eef700d20f357d68b110c5b9b193f93668ac7db8
태라 <terra2007@naver.com>
2022-10-27 10:19:20 +0900

    📑 :: send code 함수 생성

--- project/utils/user.py
+++ project/utils/user.py
@@ -1,6 +1,12 @@
 from sqlalchemy.orm import Session
 from project.core.models.user import User
 from fastapi import HTTPException, status
+from project.core.models import Redis
+from sdk.api.message import Message
+from project.core.config import API_KEY, API_SECRET, PHONE_NUMBER
+import json
+
+import random
 
 def check_id(account_id: str, session: Session):
     user = session.query(User.account_id).filter(User.account_id == account_id)
@@ -10,3 +16,24 @@ def check_id(account_id: str, session: Session):
 
     else:
         return HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+
+
+def send_code(phone_number: str):
+    code = str(random.randint(1, 9999)).zfill(4)
+    count = dict(json.loads(Redis.get(name="01034242475").decode('utf-8')))['count'] if Redis.get(name="01034242475") else 0
+    if count >= 5:
+        raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS)
+    data = {
+        "code": code,
+        "count": count + 1
+    }
+    Redis.setex(name=phone_number,
+                value=json.dumps(data, ensure_ascii=False).encode('utf-8'),
+                time=300)
+    params = dict()
+    params['type'] = 'sms'
+    params['to'] = phone_number
+    params['from'] = PHONE_NUMBER
+    params['text'] = f"[숲관] 회원가입 인증코드 입니다. : {code}"
+    Message(API_KEY, API_SECRET).send(params)
+    return HTTPException(status_code=status.HTTP_200_OK)
