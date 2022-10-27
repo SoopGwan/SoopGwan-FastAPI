9c06e27ac6ca2b4cdf066a55af8f7fb7388d8e39
태라 <terra2007@naver.com>
2022-10-27 10:50:55 +0900

    📑 :: verifycode pydantic 모델 생성

--- project/core/schemas/user.py
+++ project/core/schemas/user.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel, constr
 
 class SendCode(BaseModel):
     phone_number: constr(min_length=11, max_length=11)
+
+class VerifyCode(BaseModel):
+    phone_number: constr(min_length=11, max_length=11)
+    code: constr(min_length=4, max_length=4)
