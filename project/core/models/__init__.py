371f70053af030768c236b2741ffb8c32faff324
태라 <terra2007@naver.com>
2022-10-27 10:19:55 +0900

    📑 :: redis 설정

--- project/core/models/__init__.py
+++ project/core/models/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from project.core.config import MYSQL_DB_URL
+import redis
 
 @contextmanager
 def session_scope():
@@ -28,3 +29,5 @@ def session_scope():
         session.close()
 
 Base = declarative_base()
+
+Redis = redis.StrictRedis(host="localhost", port=6379, db=0)
