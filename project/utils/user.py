2f1dd368e805669a0820f4403b50417c6e5de4fe
태라 <terra2007@naver.com>
2022-10-28 07:53:30 +0900

    📑 : signup 함수 생성

--- project/utils/user.py
+++ project/utils/user.py
@@ -4,14 +4,19 @@ from fastapi import HTTPException, status
 from project.core.models import Redis
 from sdk.api.message import Message
 from project.core.config import API_KEY, API_SECRET, PHONE_NUMBER
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from project.core.config import JWT_ACCESS_TIMEOUT, REFRESH_ACCESS_TIMEOUT, SECRET, ALGORITHM
+import jwt
 import json
-
 import random
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def check_id(account_id: str, session: Session):
     user = session.query(User.account_id).filter(User.account_id == account_id)
 
-    if user.scalar():
+    if user.all():
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="아이디 중복")
 
     else:
@@ -35,7 +40,7 @@ def send_code(phone_number: str):
     params['to'] = phone_number
     params['from'] = PHONE_NUMBER
     params['text'] = f"[숲관] 회원가입 인증코드 입니다. : {code}"
-    Message(API_KEY, API_SECRET).send(params)
+    #Message(API_KEY, API_SECRET).send(params)
     return HTTPException(status_code=status.HTTP_200_OK)
 
 def verify_code(phone_number:str, code:str):
@@ -46,3 +51,31 @@ def verify_code(phone_number:str, code:str):
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="인증코드가 맞지 않음.")
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="인증코드 사용가능 시간이 지남.")
+
+def sign_up(phone_number: str, account_id: str, password: str, session: Session):
+    check_id(account_id=account_id, session=session)
+    session.add(
+        User(
+            phone_number=phone_number,
+            account_id=account_id,
+            password=password_hash(password)
+        )
+    )
+
+    return {
+        "access_token": create_access_token(account_id),
+        "refresh_token": create_refresh_token(account_id)
+    }
+
+def password_hash(password: str):
+    return pwd_context.hash(password)
+
+def create_access_token(account_id: str):
+    exp = datetime.utcnow() + timedelta(hours=JWT_ACCESS_TIMEOUT)
+    encoded_jwt = jwt.encode({"exp": exp, "sub": account_id}, SECRET, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(account_id: str):
+    exp = datetime.utcnow() + timedelta(hours=REFRESH_ACCESS_TIMEOUT)
+    encoded_jwt = jwt.encode({"exp": exp, "sub": account_id, "type": "refresh"}, SECRET, algorithm=ALGORITHM)
+    return encoded_jwt
