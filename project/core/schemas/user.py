304dd2789a11340e55d3427b0f3157814426b437
태라 <terra2007@naver.com>
2022-10-28 07:53:17 +0900

    📑 :: signup pydantic 모델 생성

--- project/core/schemas/user.py
+++ project/core/schemas/user.py
@@ -6,3 +6,8 @@ class SendCode(BaseModel):
 class VerifyCode(BaseModel):
     phone_number: constr(min_length=11, max_length=11)
     code: constr(min_length=4, max_length=4)
+
+class SignUp(BaseModel):
+    phone_number: constr(min_length=11, max_length=11)
+    account_id: constr(min_length=6, max_length=24)
+    password: constr(min_length=8, max_length=24)
