7e096f9002cfe675bd9acf3c8e34bea46276a13f
태라 <terra2007@naver.com>
2022-10-28 14:42:45 +0900

    📑 :: sendcode 함수 예외 처리 생성

--- project/core/schemas/user.py
+++ project/core/schemas/user.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, validator, constr
+from fastapi import HTTPException, status
 
 class SendCode(BaseModel):
-    phone_number: constr(min_length=11, max_length=11)
+    phone_number: constr()
+    @validator('phone_number')
+    def check_phone_number(cls, v):
+        if len(v) != 11 or v[:3] != "010":
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="전화번호 형식이 잘못됨.")
+        return v
 
 class VerifyCode(BaseModel):
     phone_number: constr(min_length=11, max_length=11)
