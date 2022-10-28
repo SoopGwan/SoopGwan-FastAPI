804636b36ac6b27b8d668600d3443a48c1db547e
태라 <terra2007@naver.com>
2022-10-28 14:20:10 +0900

    ♻️ :: CHAR -> String 수정

--- project/core/models/user.py
+++ project/core/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String, CHAR
+from sqlalchemy import Column, BigInteger, String
 from project.core.models import Base
 
 class User(Base):
@@ -6,5 +6,5 @@ class User(Base):
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     account_id = Column(String(24), nullable=False)
-    password = Column(CHAR(60), nullable=False)
-    phone_number = Column(CHAR(11), nullable=False)
+    password = Column(String(60), nullable=False)
+    phone_number = Column(String(11), nullable=False)
