cf444601c2fb75ef53e29c65b01ce02d8a3155a8
태라 <terra2007@naver.com>
2022-10-28 14:45:50 +0900

    📑 :: Verifycode 함수 예외 처리 생성

--- project/core/schemas/user.py
+++ project/core/schemas/user.py
@@ -10,8 +10,20 @@ class SendCode(BaseModel):
         return v
 
 class VerifyCode(BaseModel):
-    phone_number: constr(min_length=11, max_length=11)
-    code: constr(min_length=4, max_length=4)
+    phone_number: constr()
+    code: constr()
+
+    @validator('phone_number')
+    def check_phone_number(cls, v):
+        if len(v) != 11 or v[:3] != "010":
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="전화번호 형식이 잘못됨.")
+        return v
+
+    @validator('code')
+    def check_code(cls, v):
+        if len(v) != 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="인증코드 형식이 잘못됨.")
+        return v
 
 class SignUp(BaseModel):
     phone_number: constr(min_length=11, max_length=11)
