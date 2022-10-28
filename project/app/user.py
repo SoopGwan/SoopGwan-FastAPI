e0f883095efb1c0da754322624d848c22748b024
태라 <terra2007@naver.com>
2022-10-28 13:57:07 +0900

    📑 :: 회원가입 정규표현식 & HTTP 상태 코드 수정

--- project/app/user.py
+++ project/app/user.py
@@ -3,6 +3,7 @@ from project.core import session_scope
 from fastapi import APIRouter, HTTPException, status
 from project.utils.user import check_id, send_code, verify_code, sign_up
 from project.core.schemas.user import SendCode, VerifyCode, SignUp
+import re
 
 app = APIRouter()
 
@@ -20,7 +21,10 @@ async def sending_code(body: SendCode):
 async def verifying_code(body: VerifyCode):
     return verify_code(phone_number=body.phone_number, code=body.code)
 
-@app.post("/signup")
+@app.post("/signup", status_code=status.HTTP_201_CREATED)
 async def signup_user(body: SignUp):
     with session_scope() as session:
+        REGEX_PASSWORD = '^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()])[\w\d!@#$%^&*()]{8,}$'
+        if not re.fullmatch(REGEX_PASSWORD, body.password):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="비밀번호 형식이 잘못됨")
         return sign_up(phone_number=body.phone_number, account_id=body.account_id, password=body.password, session=session)
