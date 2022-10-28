2549ca69c67d7be4f227ddb948404f50680fd44c
태라 <terra2007@naver.com>
2022-10-28 20:16:21 +0900

    ♻️ :: body -> request 수정

--- project/app/user.py
+++ project/app/user.py
@@ -18,25 +18,25 @@ async def checking_id(account_id: str):
         return Response(status_code=204)
 
 @app.post("/send")
-async def sending_code(body: SendCode):
-    return send_code(phone_number=body.phone_number)
+async def sending_code(request: SendCode):
+    return send_code(phone_number=request.phone_number)
 
 @app.post("/verify")
-async def verifying_code(body: VerifyCode):
-    return verify_code(phone_number=body.phone_number, code=body.code)
+async def verifying_code(request: VerifyCode):
+    return verify_code(phone_number=request.phone_number, code=request.code)
 
 @app.post("/signup", status_code=status.HTTP_201_CREATED)
-async def signup_user(body: SignUp):
+async def signup_user(request: SignUp):
     with session_scope() as session:
-        return sign_up(phone_number=body.phone_number, account_id=body.account_id, password=body.password, session=session)
+        return sign_up(phone_number=request.phone_number, account_id=request.account_id, password=request.password, session=session)
 
 @app.post("/login")
-async def logins(body: Login):
+async def logins(request: Login):
     with session_scope() as session:
-        return login(account_id=body.account_id, password=body.password, session=session)
+        return login(account_id=request.account_id, password=request.password, session=session)
 
 @app.patch("/change", status_code=status.HTTP_204_NO_CONTENT)
-async def changing_password(body: ChangePassword, user: User = Depends(get_current_user)):
+async def changing_password(request: ChangePassword, user: User = Depends(get_current_user)):
     with session_scope() as session:
-        change_password(password=body.password, new_password=body.new_password, account_id=user.account_id, session=session)
+        change_password(password=request.password, new_password=request.new_password, account_id=user.account_id, session=session)
         return Response(status_code=204)
