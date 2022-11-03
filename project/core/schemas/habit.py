6a848769a9319ccedc2db1499a41db662fdf86e6
태라 <terra2007@naver.com>
2022-11-03 15:14:41 +0900

    📑 :: pydantic schema 생성

--- project/core/schemas/habit.py
+++ project/core/schemas/habit.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel, constr, validator
 from fastapi import HTTPException, status
+from pydantic import BaseModel, constr, validator
+
 
 class CreateHabit(BaseModel):
     content: constr()
     @validator('content')
     def check_content(cls, v):
         if not v or len(v) > 200: raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Content가 잘못됨.")
+        return v
+
+class DeleteHabit(BaseModel):
+    id: constr()
+    @validator('id')
+    def check_id(cls, v):
+        if not v or not v.isdigit(): raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="habit가 잘못됨.")
         return v
