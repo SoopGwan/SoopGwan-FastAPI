46543cf82378ab88ed3b025e345ffd942a7d48e6
태라 <terra2007@naver.com>
2022-11-03 10:58:41 +0900

    📑 :: ERD 수정 반영 entity 수정

--- project/core/models/habit_success.py
+++ project/core/models/habit_success.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, Integer, Date, ForeignKey
+from sqlalchemy import Column, BigInteger, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from project.core.models import Base
 
@@ -6,7 +6,6 @@ class HabitSucces(Base):
     __tablename__ = 'tbl_habit_success'
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    count = Column(Integer, nullable=False)
     success_at = Column(Date, nullable=False)
     week_habit_id = Column(ForeignKey('tbl_week_habit.id'), index=True)
 
