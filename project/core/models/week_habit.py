726f48884a9332d60c62f3b4664c969eec70371f
태라 <terra2007@naver.com>
2022-11-03 10:48:21 +0900

    📑 :: week_habit 엔티티 생성

---
+++ project/core/models/week_habit.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, BigInteger, String, Date, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from project.core.models import Base
+
+class WeekHabit(Base):
+    __tablename__ = 'tbl_week_habit'
+
+    id = Column(BigInteger, primary_key=True)
+    content = Column(String(200), nullable=False)
+    start_at = Column(Date, nullable=False)
+    end_at = Column(Date, nullable=False)
+    status = Column(Integer, nullable=False)
+    user_id = Column(ForeignKey('tbl_user.id'), index=True)
+
+    user = relationship('User')
