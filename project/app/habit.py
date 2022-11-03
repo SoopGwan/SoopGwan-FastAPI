d4476c3ab870fbd17f022ef90e394475d3946be2
태라 <terra2007@naver.com>
2022-11-03 14:42:05 +0900

    📑 :: creating_habit HTTP code 수정

--- project/app/habit.py
+++ project/app/habit.py
@@ -1,6 +1,6 @@
 from project.core import session_scope
 
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Response
 from project.utils.user import get_current_user
 from project.core.models.user import User
 from project.core.schemas.habit import CreateHabit
@@ -8,10 +8,8 @@ from project.utils.habit import create_habit
 
 habit_router = APIRouter()
 
-@habit_router.post("/", status_code=status.HTTP_201_CREATED)
-async def creating_habit(response: CreateHabit, user: User = Depends(get_current_user)):
+@habit_router.post("/")
+async def creating_habit(request: CreateHabit, user: User = Depends(get_current_user)):
     with session_scope() as session:
-        create_habit(content=response.content, user_id=user.id, session=session)
-        return {
-            "message": "success"
-        }
+        create_habit(content=request.content, user_id=user.id, session=session)
+        return Response(status_code=201)
