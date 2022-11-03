b16acf4990b1023cab2e111bf87a606371775162
태라 <terra2007@naver.com>
2022-11-03 14:31:56 +0900

    📑 :: router 수정

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from project.app import user
+from project.app import user, habit
 
 api_router = APIRouter()
 
 api_router.include_router(user.user_router, prefix="/users", tags=["user"])
+api_router.include_router(habit.habit_router, prefix="/habits", tags=["habit"])
